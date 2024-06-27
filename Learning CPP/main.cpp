417b0e5fe545ca461ef8aa8561d9f46b0bc640ac
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-06-27 12:14:32 -0700

    Initial Commit

---
+++ Learning CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learning CPP
+//
+//  Created by Brandon McConathy on 6/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
